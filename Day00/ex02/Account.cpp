ea7f3c571aec6974c07e6a5a55723d48b23e9a76
kLyArSo <49284497+kLyArSo@users.noreply.github.com>
2021-10-14 13:41:35 +0100

    Add files via upload

--- Day00/ex02/Account.cpp
+++ Day00/ex02/Account.cpp
@@ -8,31 +8,51 @@ int Account::_totalNbWithdrawals = 0;
 
 void	Account::makeDeposit( int deposit )
 {
+    int x = _amount;
     _totalNbDeposits++;
     _totalAmount += deposit;
     _amount += deposit;
     _nbDeposits++;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex;
+    std::cout << ";p_amount:" << x;
+    std::cout << ";deposit:" << deposit;
+    std::cout << ";amount:" << _amount;
+    std::cout << ";nb_deposits:" << _nbDeposits<<"\n";
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
+    int x = _amount;
+    _displayTimestamp();
     if (_amount >= withdrawal)
     {
         _totalNbWithdrawals++;
         _totalAmount -= withdrawal;
         _amount -= withdrawal;
         _nbWithdrawals++;
+        std::cout << "index:"<< _accountIndex;
+        std::cout << ";p_amount:"<< x;
+        std::cout << ";withdrawal:"<< withdrawal;
+        std::cout << ";amount:"<< _amount;
+        std::cout << ";nb_withdrawals:"<< _nbWithdrawals<<"\n";
         return (1);
     }
     else
+    {
+        std::cout << "index:" << _accountIndex;
+        std::cout << ";p_amount:" << x ;
+        std::cout << ";withdrawal:refused\n";
         return (0);
+    }
 }
 
 void	Account::displayStatus( void ) const
 {
     _displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";"<< "amount:"<< _amount << ";";
-	std::cout << "deposits:" << _nbDeposits << ";"<< "withdrawals:"<< _nbWithdrawals <<";\n";
+	std::cout << "deposits:" << _nbDeposits << ";";
+    std::cout << "withdrawals:"<< _nbWithdrawals <<"\n";
 }
 void	Account::displayAccountsInfos( void )
 {
@@ -73,5 +93,6 @@ Account::Account( int initial_deposit )
 }
 Account::~Account()
 {
+    _displayTimestamp();
     std::cout << "index:" << _accountIndex << ";"<< "amount:"<< _amount <<";closed\n";
 }
